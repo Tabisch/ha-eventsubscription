6df2546775c1f6458ba662b355e2ac3abcf7dd64
Tobias Thiemann <tobithieman@gmail.com>
2024-02-22 00:17:39 +0100

    asdasdasd

--- custom_components/eventsubscription/config_flow.py
+++ custom_components/eventsubscription/config_flow.py
@@ -14,23 +14,10 @@ class EventSubscriptionFlow(ConfigFlow, domain=DOMAIN):
 
     VERSION = 1
 
-    async def async_step_user(
-        self, user_input: dict[str, Any] | None = None
-    ) -> FlowResult:
+    async def async_step_user(self, user_input: dict[str, Any] | None = None) -> FlowResult:
         """Handle a flow initialized by the user."""
-        # Check if already configured
+        
         await self.async_set_unique_id(DOMAIN)
         self._abort_if_unique_id_configured()
 
-        if user_input is not None:
-            return self.async_create_entry(title="EventSubscription", data={})
-
-        return self.async_show_form(step_id="user")
-
-    async_step_import = async_step_user
-
-    async def async_step_onboarding(
-        self, _: dict[str, Any] | None = None
-    ) -> FlowResult:
-        """Handle a flow initialized by onboarding."""
-        return await self.async_step_user(user_input={})
+        return self.async_create_entry(title="EventSubscription", data={})
